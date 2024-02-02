8350331fbfd6e79fff99c2901c85da428c3912c3
armandojuliomartinez <armandoj@hawaii.edu>
2024-02-01 15:19:11 -1000

    finished browser hist6

---
+++ Lab5/exercise1.js
@@ -0,0 +1,8 @@
+age = 20;
+fav_num = 6;
+day_of_birth = 20;
+month_of_birth = 8;
+console.log(
+    age + fav_num / day_of_birth * month_of_birth,
+    (age + fav_num) / day_of_birth * month_of_birth
+    )
