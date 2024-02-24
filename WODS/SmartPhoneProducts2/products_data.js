9d139adcd6deaba1d0e8e7887b219ad706b00885
armandojuliomartinez <armandoj@hawaii.edu>
2024-02-23 22:21:07 -1000

    Finished Smartphone products2

---
+++ WODS/SmartPhoneProducts2/products_data.js
@@ -0,0 +1,26 @@
+console.log("product variables loaded..."); 
+
+//product 1
+let name1 = "HTC";
+let price1 = 40.00;
+let image1 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg";
+
+//product 2
+let name2 = "Apple";
+let price2 = 75.00;
+let image2 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg";
+
+//product 3
+let name3 = "Nokia";
+let price3 = 35.00;
+let image3 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg";
+
+//product 4
+let name4 = "Samsung";
+let price4 = 45.00;
+let image4 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg";
+
+// product 5
+let name5 = "Blackberry";
+let price5 = 10.00;
+let image5 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg";
