eb38bb09c2d6b2195aa084b7f5ecc3a21d355b21
armandojuliomartinez <armandoj@hawaii.edu>
2024-03-28 20:32:07 -1000

    Finished Store1 wod

---
+++ WODS/Store1/products_data.js
@@ -0,0 +1,34 @@
+console.log("product variables loaded..."); 
+
+let product1 = {brand: "HTC",
+    price: 40.00,
+    image:"http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg"
+};
+
+let product2 = {
+    brand: "Apple",
+    price: 75.00,
+    image:"http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg"
+};
+
+let product3 = {
+    brand: "Nokia",
+    price: 35.00,
+    image:"http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg",
+};
+
+let product4 = {
+    brand: "Samsung",
+    price: 45.00,
+    image:"http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg",
+};
+
+let product5 = {
+    brand: "Blackberry",
+    price: 10.00,
+    image:"http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg",
+}
+//An array that contains the different products
+let products=[
+    product1, product2, product3, product4, product5
+]
